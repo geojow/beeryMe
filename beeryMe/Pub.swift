2311c775a39cb27417938c494208adc4b92d84e7
George Jowitt <geojow@hotmail.co.uk>
2017-11-12 18:30:49 +0000

    Added id variable to Pub class

--- beeryMe/Pub.swift
+++ beeryMe/Pub.swift
@@ -11,12 +11,14 @@ import MapKit
 
 class Pub: NSObject {
     
+    
+    let id: Int
     let name: String
     let location: CLLocation
     var visited: Bool
     
-    init(name: String, latitude: Double, longitude: Double, visited: Bool) {
-        
+    init(id: Int, name: String, latitude: Double, longitude: Double, visited: Bool) {
+        self.id = id
         self.name = name
         self.location = CLLocation(latitude: latitude, longitude: longitude)
         self.visited = visited
